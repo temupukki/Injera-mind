e2cc80554605ed9110993407cc1a05fdcb5db6f0
Temesgen Gashaw <temesgengashaw8@gmail.com>
2026-02-15 10:16:42 +0300

    edit-footer

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 
 export default function Footer(){
     return(
-          <footer className="border-t border-orange-100 py-8 px-4">
+          <footer className="border-t border-orange-100 py-8 px-4 bg-amber-50">
         <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center text-gray-600 text-sm">
           <div className="flex items-center gap-2 mb-4 sm:mb-0">
             <Utensils className="text-orange-500" size={20} />
